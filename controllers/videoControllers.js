12cac7105faa637791158fcfed70dfbbb6b5c045
Okononfua Mathew <oseghaleokononfua@gmail.com>
2023-09-29 23:20:45 +0100

    All features added

--- controllers/videoControllers.js
+++ controllers/videoControllers.js
@@ -9,25 +9,82 @@ exports.getVideo = (req, res, next) => {
     // process.chdir(newDirectoryPath);
     const videoPath = path.join(videoDirectory, filename);
     console.log(videoDirectory);
-    // Check if the video file exists and is readable
-    fs.access(videoPath, fs.constants.F_OK | fs.constants.R_OK, (err) => {
-        if (err) {
-            // If there's an error, it means the file doesn't exist or is not readable
-            // res.status(404).send('Video not found');
-            console.log(err);
-        } else {
-            // The file exists and is readable
-            res.status(200).json({ message: 'Video exists', videoUrl: videoPath });
-        }
-    });
+     // Check if the video file exists
+     if (fs.existsSync(videoPath)) {
+        // Use the 'video/mp4' MIME type for video files (adjust as needed)
+        res.setHeader('Content-Type', 'video/mp4');
+        
+        // Stream the video file to the response
+        const videoStream = fs.createReadStream(videoPath);
+        videoStream.pipe(res);
+    } else {
+        // Return a 404 error if the video file does not exist
+        res.status(404).send('Video not found');
+    }
 
 };
 
-exports.uploadVideo = (req, res, next) => {
-    if(!req.file){
-        return res.status(400).json({error: 'No file uploaded'})
-    }
+exports.uploadVideo = async (req, res, next) => {
+    try {
+        if (req.file) {
+            // If there is a file in the request, it's a standard upload
+            const videoPath = `${req.protocol}://${req.get('host')}/uploads/videos/${req.file.filename}`;
+            return res.status(200).json({ success: true, link: videoPath });
+        }
 
-    const videoPath = `/uploads/videos/${req.file.filename}`;
-    return res.status(200).json({success:true, videoPath})
-}
+        // If there's no file, it's a chunked upload
+        // You should handle chunked uploads here, such as assembling the chunks
+        // and saving the complete video to a permanent location
+
+        // Check if it's a chunk upload or the finalization request
+        if (req.body.finalize) {
+            // Handle the request to finalize the upload
+            // First, append any remaining chunk data (if needed)
+            const chunkData = req.body.chunkData; // Assuming you have a 'chunkData' field in your request
+            if (chunkData) {
+                fs.appendFile('tempVideo.mp4', chunkData, (err) => {
+                    if (err) {
+                        console.error('Error appending video chunk:', err);
+                        return res.status(500).json({ error: 'Error uploading video chunk' });
+                    }
+
+                    // After appending, proceed to finalize by renaming
+                    fs.rename('tempVideo.mp4', `uploads/videos/${req.file.filename}`, (err) => {
+                        if (err) {
+                            console.error('Error finalizing video upload:', err);
+                            return res.status(500).json({ error: 'Error finalizing video upload' });
+                        } else {
+                            const videoPath = `${req.protocol}://${req.get('host')}/uploads/videos/${req.file.filename}`;
+                            return res.status(200).json({ success: true, link: videoPath });
+                        }
+                    });
+                });
+            } else {
+                // If there's no remaining chunk data, directly rename the file
+                fs.rename('tempVideo.mp4', `uploads/videos/${req.file.filename}`, (err) => {
+                    if (err) {
+                        console.error('Error finalizing video upload:', err);
+                        return res.status(500).json({ error: 'Error finalizing video upload' });
+                    } else {
+                        const videoPath = `${req.protocol}://${req.get('host')}/uploads/videos/${req.file.filename}`;
+                        return res.status(200).json({ success: true, link: videoPath });
+                    }
+                });
+            }
+        } else {
+            // Handle individual video chunk uploads
+            const chunkData = req.body.chunkData; // Assuming you have a 'chunkData' field in your request
+            fs.appendFile('tempVideo.mp4', chunkData, (err) => {
+                if (err) {
+                    console.error('Error appending video chunk:', err);
+                    return res.status(500).json({ error: 'Error uploading video chunk' });
+                } else {
+                    return res.status(200).json({ success: true, message: 'Video chunk uploaded successfully' });
+                }
+            });
+        }
+    } catch (error) {
+        console.error('Error uploading video:', error);
+        return res.status(500).json({ error: 'An error occurred while uploading' });
+    }
+};
--- routes/videoRoutes.js
+++ routes/videoRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
 
 const { getVideo, uploadVideo } = require('../controllers/videoControllers')
 
@@ -25,6 +26,39 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({ storage, fileFilter});
 
 const router = express.Router();
+
+// Directory where your uploaded videos are stored
+const videoDirectory = 'uploads/videos'; // Adjust the path as needed
+
+// Route to get a list of all video files
+router.get('/api/videos', (req, res) => {
+    try {
+        // Read the contents of the video directory
+        fs.readdir(videoDirectory, (err, files) => {
+            if (err) {
+                console.error('Error reading video directory:', err);
+                return res.status(500).json({ error: 'An error occurred while fetching videos' });
+            }
+
+            // Filter out non-video files (adjust as needed based on file types)
+            const videoFiles = files.filter((file) => {
+                const fileExtension = path.extname(file).toLowerCase();
+                return ['.mp4', '.avi', '.mkv'].includes(fileExtension); // Add more extensions if necessary
+            });
+
+            // Construct an array of video URLs
+            const videoUrls = videoFiles.map((file) => {
+                return `${req.protocol}://${req.get('host')}/uploads/videos/${file}`;
+            });
+
+            return res.status(200).json({ success: true, videos: videoUrls });
+        });
+    } catch (error) {
+        console.error('Error fetching videos:', error);
+        return res.status(500).json({ error: 'some error occurred while fetching videos' });
+    }
+});
+
 
 router.get('/api/video/:filename', getVideo);
 
