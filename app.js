979542fb8efef18ca9f44ec5f2fc71c0908e66b2
Okononfua Mathew <oseghaleokononfua@gmail.com>
2023-09-30 09:27:53 +0100

    All video endpoints added

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const videoRoutes = require('./routes/videoRoutes')
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+
 app.use(videoRoutes);
 
 app.use(express.json());
